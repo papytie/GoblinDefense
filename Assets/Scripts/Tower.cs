5e3e4dd51e6bbcb1709da423cc087eb0278f4b1c
papytie <86955965+papytie@users.noreply.github.com>
2023-12-22 15:27:21 +0100

    Update Tower.cs
    thx to MaxDo <3

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -5,35 +5,47 @@ using UnityEngine;
 
 public class Tower : MonoBehaviour
 {
-    public event Action OnEnemiesInRange = null; // STEP 1
-    public event Action OnTargetSelection = null; // STEP 2
-    public event Action OnTargetAcquired = null; // STEP 3
-    public event Action OnProjectileSpawn = null; // STEP 4
-    public event Action OnTargetExpired = null; // CANCELATION
+    public bool IsAttacking
+    {
+        get => isAttacking;
+        set
+        {
+            isAttacking = value;
+            towerAnimation.UpdateIsAttackingBoolParam(value); // Update Anim value
+        }
+    }
+    public Entity MainTarget => mainTarget;
 
+    // The tower attack behaviour use a sequence of events : ( below)
+    event Action OnEnemiesInRange = null;
+    event Action OnTargetSelection = null;
+    event Action OnTargetAcquired = null;
+    event Action OnProjectileSpawn = null;
+    event Action OnTargetExpired = null;
+
+    [Header("References")]
     [SerializeField] Projectiles projToSpawn = null;
     [SerializeField] Entity mainTarget = null;
     [SerializeField] TowerAnimation towerAnimation = null;
 
+    [Header("Settings")]
     [SerializeField, HideInInspector] List<Entity> entitiesInRange = new();
     [SerializeField] float towerRange = 20; // Entity detection range
     [SerializeField] float fireRate = 1; // time between each projectile spawn
     [SerializeField] float animationDuration = 1.66f; // set animation legth to proper synchro with fire rate
     [SerializeField] float detectionRate = 1f; // time between each detection loop
     [SerializeField] bool isAttacking = false; // switch to prevent multi attacking on a target
-    public bool IsAttacking => isAttacking;
-    public Entity MainTarget => mainTarget;
 
     void Start()
     {
-        Init();
+        InitTower();
     }
 
-    void Init()
+    void InitTower()
     {
         towerAnimation = GetComponent<TowerAnimation>();
         towerAnimation.UpdateFireRateFloatValue(fireRate);
-        
+
         EnableDetection(); // Step 0 : check for Entities in range
 
         OnEnemiesInRange += () => // Step 1 : Choose a target & disable detection
@@ -49,125 +61,77 @@ public class Tower : MonoBehaviour
         OnTargetExpired += () => // CANCELATION : Basically reset to Step 0
         {
             mainTarget = null;
-            UpdateIsAttackingValue(false);
+            IsAttacking = false;
             EnableDetection();
         };
         OnTargetAcquired += () => // Step 3 : Begin Draw Arrow Animation & switch isAttacking
         {
             BeginFire();
             towerAnimation.UpdateAttackTriggerParam();
-            UpdateIsAttackingValue(true);
+            IsAttacking = true;
         };
         OnProjectileSpawn += () => // Step 4 : switch isAttacking and resume to Step 2
         {
-            UpdateIsAttackingValue(false);
+            IsAttacking = false;
             CheckTarget();
         };
-    }
+    }// Add Functions to essentials events
 
     void EntityDetection()
     {
         // cancel check if list is empty
-        if (EnemyManager.Instance.AllEntitiesInScene.Count == 0) return; 
+        if (EnemyManager.Instance.AllEntitiesInScene.Count == 0) return;
 
-        // Clear EntityInRange to remove missing objects when an entity is destroyed
-        entitiesInRange.Clear(); 
-        
+        entitiesInRange.Clear(); //To remove missing objects when an entity is destroyed
+
         if (mainTarget == null)
         {
-            // check all Entity in list
-            foreach (Entity _entity in EnemyManager.Instance.AllEntitiesInScene) 
+            foreach (Entity _entity in EnemyManager.Instance.AllEntitiesInScene) // check all Entity in list
             {
-                // If in range add to list
-                if (InRange(TowerPos(this), 
-                    EntityPos(_entity), 
-                    towerRange) 
-                    && !_entity.IsDead) 
-                        AddToList(_entity); 
-            }
-            if (entitiesInRange.Count == 0) return;
-            OnEnemiesInRange?.Invoke(); // STEP 1
-        }  
-    }
+                if (_entity.IsDead || entitiesInRange.Contains(_entity)) continue;
 
-    void PickTarget() 
+                float _dist = Vector3.Distance(transform.position, _entity.transform.position);
+                if (_dist < towerRange)
+                    entitiesInRange.Add(_entity); // If in range add to list
+            }
+
+            if (entitiesInRange.Count > 0)
+            {
+                OnEnemiesInRange?.Invoke(); // STEP 1
+            }
+        }
+    }// Used to check entities in range, create list and begin attack sequence
+
+    void PickTarget()
     {
         if (entitiesInRange.Count == 0) return;
+
         mainTarget = entitiesInRange[0];
         OnTargetSelection?.Invoke(); // STEP 2
-    }
+    }// Take the first element of the list as mainTarget
 
     void CheckTarget()
     {
-        if (!InRange(TowerPos(this), 
-            EntityPos(mainTarget), 
-            towerRange) 
-            || mainTarget.IsDead)
-            {
-                OnTargetExpired?.Invoke(); // CANCELATION
-                return;
-            }
+        if (Vector3.Distance(transform.position, mainTarget.transform.position) > towerRange || mainTarget.IsDead)
+        {
+            OnTargetExpired?.Invoke(); // CANCELATION
+            return;
+        }
         OnTargetAcquired?.Invoke(); // STEP 3
-    }
+    }// Check that mainTarget is alive and in range
 
     void BeginFire()
     {
         if (!mainTarget || isAttacking) return;
         Invoke(nameof(SpawnArrow), animationDuration / fireRate);
-    }
+    }// Begin animation and Invoke delayed projectile
 
     void SpawnArrow()
     {
-        Projectiles _proj = Instantiate(projToSpawn, 
-            transform.position + transform.up * 2, 
-            transform.rotation);
+        Projectiles _proj = Instantiate(projToSpawn, transform.position + transform.up * 2, transform.rotation);
         OnProjectileSpawn?.Invoke(); // STEP 4
         _proj.SetTowerTargetRef(this);
-    }
-
-    #region Utilities
-    void EnableDetection() // Run EntityDetection each .x sec instead of Update, less performance used
-    {
-        InvokeRepeating(nameof(EntityDetection), 0, detectionRate);
-    }
-
-    void DisableDetection()
-    {
-        CancelInvoke(nameof(EntityDetection));
-    }
-
-    bool InList(Entity _entityToCheck) // simple check
-    {
-        return entitiesInRange.Contains(_entityToCheck); 
-    }
-
-    void AddToList(Entity _entityToAdd) // Check and Add if not already in list
-    {
-        if(!InList(_entityToAdd))
-        entitiesInRange.Add(_entityToAdd);
-    }
-
-    void RemoveFromList(Entity _entityToAdd) // Check and Remove if in list
-    {
-        if(InList(_entityToAdd))
-        entitiesInRange.Remove(_entityToAdd);
-    }
-
-    bool InRange(Vector3 _from, Vector3 _to, float _range)
-    {
-        if (Vector3.Distance(_from, _to) < _range) return true;
-        else return false;
-    }
-
-    Vector3 EntityPos(Entity _entity)
-    {
-        return _entity.transform.position;
-    }
-
-    Vector3 TowerPos(Tower _tower)
-    {
-        return _tower.transform.position;
-    }
+    }// Spawn projectile and end attack sequence
 
     void FollowTarget()
     {
@@ -177,14 +141,18 @@ public class Tower : MonoBehaviour
             return;
         }
         transform.LookAt(mainTarget.transform.position, transform.up);
+    }// LookAt Target or cancel own Invoke
+
+    void EnableDetection()
+    {
+        InvokeRepeating(nameof(EntityDetection), 0, detectionRate);
     }
 
-    void UpdateIsAttackingValue(bool _value)
+    void DisableDetection()
     {
-        isAttacking = _value;
-        towerAnimation.UpdateIsAttackingBoolParam(isAttacking); // Update Anim value
+        CancelInvoke(nameof(EntityDetection));
     }
-    #endregion Utilities
+
 
     // -------- DEBUG ---------
     private void OnDrawGizmos()
